a920563819fdc6422c1aa6128e4f237e5f134491
eurekasolution <kjmin@daum.net>
2023-12-06 16:14:28 +0900

    C16:14

--- brute.php
+++ brute.php
@@ -1,3 +1,45 @@
 <?php
+    $letters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    $size = strlen($letters);
+    
+    // abcd
+    echo "size = $size<br>";
+    $cnt = 0;
 
+    for($i=0; $i< $size; $i++)
+    {
+        for($j=0; $j < $size; $j ++)
+        {
+            for($k=0; $k<$size; $k++)
+            {
+                for($l = 0; $l < $size; $l++)
+                {
+                    $cnt ++;
+
+                    if($cnt % 10000 == 0)
+                    {
+                        echo "$cnt <br>";
+                    }
+
+                    $pw = $letters[$i] . $letters[$j] . $letters[$k] . $letters[$l];
+                    $sql = "select * from users where pass='$pw'";
+                    $result = mysqli_query($conn, $sql);
+                    $data = mysqli_fetch_array($result);
+
+                    if($data)
+                    {
+                        while($data)
+                        {
+                            $id = $data["id"];
+                            echo "id = $id, pass = $pw<br>";
+                            $data = mysqli_fetch_array($result);
+                        }
+                    }
+
+                    if($cnt > 500000)
+                        exit();
+                }
+            }
+        }
+    }
 ?>
