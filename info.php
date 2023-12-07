d95f07e5d27b0e10a9288be00f0aa39207bbecc7
eurekasolution <kjmin@daum.net>
2023-12-07 16:17:41 +0900

    D16:20

--- info.php
+++ info.php
@@ -1,6 +1,27 @@
 <div class="row">
     <div class="col colLine">
         <?php
+
+            $file1 = "a.test.jpg";
+            $file2 = "README";
+
+            $split = explode(".", $file1);
+            $count = count($split);
+            echo "count = $count <br>"; // 3
+            //$tmp = $count -1;
+            echo "ext = " . $split[$count -1] . "<br>";
+
+            $split = explode(".", $file2);
+            $count = count($split);
+            echo "count = $count <br>"; // 1
+            echo "ext = " . $split[$count -1] . "<br>";
+
+            if($count == 1)
+                echo "ext = NONE<br>";
+
+
+
+
             phpinfo();
         ?>
     </div>
